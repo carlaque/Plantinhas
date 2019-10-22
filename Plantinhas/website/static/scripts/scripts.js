d8fb19f498b35b3d2183cccd211e5125959504bf
ivemotta <ive_motta@hotmail.com>
2019-10-22 18:59:22 -0300

    Terminar css do index e login

--- Plantinhas/website/static/scripts/scripts.js
+++ Plantinhas/website/static/scripts/scripts.js
@@ -2,6 +2,7 @@ let i = 0;
 let lista = [];
 let time = 2000;
 let section = document.getElementById('sono');
+let div = document.getElementsByClassName('ache_uma_planta')
 
 lista[0] = "ter acesso a uma alimentação sem agrotóxicos";
 lista[1] = "atividade terapêutica para aliviar o stress do dia a dia";
@@ -27,4 +28,13 @@ window = slideShow()
 function mais(codigo){
     var x = document.querySelector(".info")
     x.style.visibility = "visible"
+}
+
+
+function clicar_planta(){
+    window.location = "plantas/0"
+}
+
+function clicar_jardim(){
+    window.location = "jardim/cadastro/1"
 }
