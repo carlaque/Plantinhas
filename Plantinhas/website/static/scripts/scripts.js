beac1f2adf975424dca766c1d7b411d03b20b7d5
ivemotta <ive_motta@hotmail.com>
2019-10-22 03:46:18 -0300

    fazer section e slideshow com javascript

---
+++ Plantinhas/website/static/scripts/scripts.js
@@ -0,0 +1,24 @@
+let i = 0;
+let lista = [];
+let time = 2000;
+let section = document.getElementById('sono');
+
+lista[0] = "ter acesso a uma alimentação sem agrotóxicos";
+lista[1] = "atividade terapêutica para aliviar o stress do dia a dia";
+lista[2] = "criar um ambiente bonito e agradável dentro de casa";
+lista[3] = "desenvolver hábitos alimentares mais saudáveis";
+lista[4] = "aumentar o contato com a natureza";
+lista[5] = "diminuir a dependência da indústria alimentícia";
+
+function slideShow(){
+    section.innerHTML = lista[i];
+    if(i < lista.length - 1){
+        i++;
+    }
+    else{
+        i = 0;
+    }
+    setTimeout('slideShow()', time);
+}
+
+window = slideShow()
