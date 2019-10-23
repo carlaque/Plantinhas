931a27bca6194f799e5a480058f5288dcfbf3383
carla andrade <andrade.carla12@gmail.com>
2019-10-23 19:13:47 -0300

    heroku

--- Plantinhas/plantinhas/settings.py
+++ Plantinhas/plantinhas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'h_75dbx=%!cppolb-ehts)g)))(-5m&g%@98v9(7ovy@_%a^g#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'plantinhas.herokuapp.com']
 
 
 # Application definition
@@ -118,4 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+django_heroku.settings(locals())
